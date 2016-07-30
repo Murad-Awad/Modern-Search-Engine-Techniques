2225affe6ac4ddcc05b9e693a944aeb40a3bd4e5
MuradAwad <Murad.bummos@gmail.com>
2016-07-29 22:01:17 -0700

    Added a page ranking function based on 3 criteria regarding a query

---
+++ src/Rank.py
@@ -0,0 +1,34 @@
+importance=[]
+def Parse(directory, query):
+    freqAllWords = 0
+    freqOverall = 0
+    counter = 0
+    freqEachWord=[]
+    file = open(directory, 'r')
+    words = query.split()
+    for line in file:
+        for word in words:
+            if word in line:
+                freqAllWords += 1
+        if query in line:
+            freqOverall+=1
+    importance.append(freqOverall)
+    importance.append(freqAllWords)
+def Parse2(directory,query):
+    counter=0
+    freqEachWord = []
+    file=open(directory, 'r')
+    words = query.split()
+    for word in words:
+        counter+=1
+    for line in file:
+        for i in range(0, counter):
+            freqEachWord.append(0)
+            if words[i] in line:
+                freqEachWord[i]+=1
+    freqEachWord=freqEachWord[0:counter]
+    importance.append(freqEachWord)
+
+Parse("/home/murad/textfile", "report cost")
+Parse2("/home/murad/textfile", "report cost")
+print (importance)
